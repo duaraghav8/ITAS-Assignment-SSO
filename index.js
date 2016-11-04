ba172b0494a775207e0829ff70657b6c76da51f0
duaraghav8@gmail <duaraghav8@gmail.com>
2016-11-04 23:28:36 +0530

    basic

--- index.js
+++ index.js
@@ -4,3 +4,33 @@
  */
 
 'use strict';
+
+const express = require ('express'),
+	https = require ('https'),
+
+	serveStatic = require ('serve-static'),
+	bodyParser = require ('body-parser'),
+
+	fs = require ('fs'),
+	path = require ('path');
+
+let app = express ();
+/*
+let httpsServer = https.createServer ({
+
+	key: fs.readFileSync (path.join (__dirname, 'ssl/server.key'), 'utf8'),
+	cert: fs.readFileSync (path.join (__dirname, 'ssl/server.csr'), 'utf8'),
+	passphrase: 'butterchicken'
+
+}, app);
+*/
+
+let listener = app.listen (process.env.PORT || 8000, () => {
+	console.log ('Listening on port: ' + listener.address ().port);
+});
+
+/*
+let listener = httpsServer.listen (process.env.PORT || 8000, () => {
+	console.log ('Listening on port: ', listener.address ().port);
+});
+*/
